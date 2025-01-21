d130a358e2b1bc667985d8dcb6e18b91fa38c9d1
Michael Bricout <bricout.michael@gmail.com>
2025-01-21 17:36:49 -0500

    Complete index.html CSS + accessibility

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -7,11 +7,27 @@ function photographerTemplate(data) {
         const article = document.createElement( 'article' );
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        img.setAttribute("alt", "Portrait de "+name);
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const div = document.createElement('div');
+        div.className = "subtitle";
+        const location = document.createElement('p');
+        location.className = "geo";
+        location.textContent = city+", "+country;
+        const slogan = document.createElement('p');
+        slogan.className = "tag";
+        slogan.textContent = tagline;
+        const priceDisplay = document.createElement('p');
+        priceDisplay.className = "price";
+        priceDisplay.textContent = price+"€/jour";
+        div.appendChild(location);
+        div.appendChild(slogan);
+        div.appendChild(priceDisplay);
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(div);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, city, country, tagline, price, getUserCardDOM }
 }
